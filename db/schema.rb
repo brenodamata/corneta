c8970711d12a1ba964e3c856f77f833a5215e6dc
brenodamata <bjrdamata@gmail.com>
2016-10-04 21:09:09 -0400

    Add field: Url to Posts.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160706230451) do
+ActiveRecord::Schema.define(version: 20161005010821) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -90,6 +90,7 @@ ActiveRecord::Schema.define(version: 20160706230451) do
     t.integer  "admin_user_id"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.string   "url"
   end
 
   add_index "posts", ["admin_user_id"], name: "index_posts_on_admin_user_id", using: :btree
